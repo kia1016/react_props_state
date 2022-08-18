21d710cb5add977f5a987403c689c3f4148893be
kia1016 <kia1016@gmail.com>
2022-08-18 19:40:44 -0400

    React_Props_State

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { Component } from "react";
-
+import Box from "./components/Box";
 class App extends Component {
   constructor(props) {
     super(props);
-    
+
     const BOX_COUNT = 24;
     const BOXES = new Array(BOX_COUNT);
     
@@ -16,11 +16,35 @@ class App extends Component {
     }
 
     // set default state
-
+      this.state = {boxes:BOXES};
     // bind methods to this
+    this.handleBoxClick = this.handleBoxClick.bind(this);
   }
+  getRandomColor() {
+    const rgb = Math.floor(Math.random() * 256);
+    return rgb;
+   }
+
+    handleBoxClick(e) {
+      const newBoxes = this.state.boxes.map((box) => {
+        if (box.id == e.target.id) {
+          box.color = `rgb(${this.getRandomColor()}, ${this.getRandomColor()}, ${this.getRandomColor()})`;
+        }
+        return box;
+      });
+
+      this.setState({ boxes: newBoxes });
+    }
 
   render() {
+    const renderBoxes = this.state.boxes.map((box) => (
+      <Box
+        key={box.id}
+        id={box.id}
+        color={box.color}
+        handleClick={this.handleBoxClick}
+      />
+    ));
     return (
       <main
         style={{
@@ -31,7 +55,7 @@ class App extends Component {
         }}
       >
         <h1>React: State and Props</h1>
-        <div className="App">{/* render boxes */}</div>
+        <div className="App">{renderBoxes}</div>
       </main>
     );
   }
